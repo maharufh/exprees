25e15082615b818d4a73a22beee33343577021e6
Maharufh Ali Mir <85804873+maharufh@users.noreply.github.com>
2022-09-07 14:44:24 +0530

    Add files via upload

---
+++ Express Js/blogExpress/data/blogs.js
@@ -0,0 +1,26 @@
+blogs = {
+    blogs: [
+        {
+            title: "How to get started with python",
+            content: "This is content python",
+            slug:"Python-learn"
+        },
+        {
+            title: "How to get started with Js",
+            content: "This is content js",
+            slug:"js-learn"
+        },
+        {
+            title: "How to get started with Django",
+            content: "This is content Django",
+            slug:"Django-learn"
+        },
+        {
+            title: "How to get started with css",
+            content: "This is content css",
+            slug:"css-learn"
+        },
+    ]
+}
+
+module.exports = blogs;
---
+++ Express Js/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+
+// const harryMiddleware = (req,res,next)=>{
+//    console.log(req)
+//    next()
+// }
+app.use(express.static(path.join(__dirname, "public")))
+// app.use(harryMiddleware)
+
+app.get('/hello/:name', (req, res) => {
+    res.send('Hello World!', + req.params.name)
+})
+app.get('/about', (req, res) => {
+    //   res.send('about');
+    // res.sendFile(path.join(__dirname, 'index.html'))
+    res.json({ "harry": 34 })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
